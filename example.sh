5058dac6887912eaf21f2bf5f2d274a30b134072
tchapi <regbasket@gmail.com>
2013-07-29 18:42:59 +0200

    Example script does not need to debug

--- example.sh
+++ example.sh
@@ -2,7 +2,7 @@
 # Standard bootSHtrap script template example
 
 # Use BootSHtrap
-__DEBUG=1 # Sets the debug mode, which outputs logs to standard output
+# __DEBUG=1 # Sets the debug mode, which outputs logs to standard output
 source bootshtrap/autoload.sh # Autoloads the whole stuff
 
 # You need to have a main() function in your script - this is your entry point
